1fee88297ae6d5399d293fc9748f6fcb2c37d788
Akshatoff <akshatascoder@gmail.com>
2025-07-16 17:20:22 +0530

    Updated type shi

--- script/main.js
+++ script/main.js
@@ -2,6 +2,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger, SplitText, TextPlugin, DrawSVGPlugin);
 
   let heading_split = new SplitText("#home-heading", { type: "chars" });
+  const card = document.querySelector('.code-snip')
+  let bounds;
+  let pyrocon = document.querySelector(".pyro-image-con");
+  let pyroimg = document.querySelector(".pyro-img ");
+
+  function rotateToMouse(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const leftX = mouseX - bounds.x;
+    const topY = mouseY - bounds.y;
+    const center=  {
+      x: leftX - bounds.width / 2,
+      y: topY - bounds.height / 2,
+    }
+    const distance = Math.sqrt(center.x**2 + center.y**2)
+
+    card.style.transform = `
+  rotate3d(
+    ${center.y / 100},
+    ${-center.x / 100},
+    0,
+    ${Math.log(distance) * 4}deg
+  )`;
+
+
+  }
+
+  card.addEventListener('mouseenter', () => {
+    bounds = card.getBoundingClientRect();
+    document.addEventListener('mousemove', rotateToMouse);
+  });
+
+  card.addEventListener('mouseleave', () => {
+    document.removeEventListener('mousemove', rotateToMouse);
+    card.style.transform = '';
+    card.style.background = '';
+  });
   let quote_split = new SplitText("#home-quote", {
     type: "chars",
   });
@@ -35,7 +72,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         start: "300% top",
         end: "+=800",
         scrub: true,
-        markers: true,
       },
     });
 
@@ -61,7 +97,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         start: "200% top",
         end: "+=800",
         scrub: true,
-        markers: true,
       },
     });
 
@@ -79,6 +114,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
       }
     );
   });
+  pyrocon.addEventListener("mousemove", (e) => {
+    const rect = pyrocon.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const offsetX = x - centerX;
+    const offsetY = y - centerY;
+
+
+      const moveX = offsetX * 0.25;
+      const moveY = offsetY * 0.25;
+
+        pyroimg.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.1)`;
+
+  });
+pyrocon.addEventListener("mouseleave", () => {
+    pyroimg.style.transform = `translate(0px, 0px) scale(1)`;
+
+});
+
+pyrocon.addEventListener("mouseenter", () => {
+  pyroimg.style.transform = `scale(1.05)`;
+})
+
 });
 
 const lenis = new Lenis();
